db6627250cc7e10965a2a8f970e6c94aa6f1e7f0
Azees253 <mohamed61@gmail.com>
2025-03-10 17:21:39 +0530

    updates

--- src/componant/Contact.jsx
+++ src/componant/Contact.jsx
@@ -47,7 +47,7 @@ export default function Contact() {
             <a href="https://www.instagram.com/azees706/?hl=en">
               <i class="fa-brands fa-instagram"></i>
             </a>
-            <a href="">
+            <a href="mailto:mohamedazees61@gmail.com">
               <i class="fa-solid fa-envelope"></i>
             </a>
           </div>
