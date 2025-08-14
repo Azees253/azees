07b50b09d74ae0211b4d399be73d3add18c5ccc4
Azees253 <mohamed61@gmail.com>
2025-08-14 19:00:05 +0530

    updates

--- main.js
+++ main.js
@@ -18,3 +18,17 @@ function handleClose() {
 navLinks.forEach((link) => {
   link.addEventListener("click", () => (sidenav.style.right = "-60%"));
 });
+
+function SendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email_form: document.getElementById("email_form").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+  emailjs
+    .send("service_pysdv3m", "template_4dtiprc", params)
+    .then(function (res) {
+      alert("Your message has been sent successfully!" + res.status);
+    });
+}
